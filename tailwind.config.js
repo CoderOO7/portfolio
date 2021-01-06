054da37c0a59059365588bc6b3e006e2ca9fe522
CoderOO7 <shubhamyadav.70007@outlook.com>
2021-01-06 19:44:09 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    purge: [
+      './public/**/*.html',
+      './public/**/*.js',
+    ],
+     darkMode: false, // or 'media' or 'class'
+     theme: {
+       extend: {},
+     },
+     variants: {},
+     plugins: [],
+}
