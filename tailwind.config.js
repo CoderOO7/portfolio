52acf867b7eeec13c9c9eec0b02e5ea17f6072e2
coderoo7 <shubhamyadav.70007@gmail.com>
2021-03-31 12:53:39 +0530

    chore:Build Script changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,45 @@
 // tailwind.config.js
-const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors');
 module.exports = {
-    purge: [
-      './public/**/*.html',
-      './public/**/*.js',
-    ],
-     darkMode: false, // or 'media' or 'class'
-     theme: {
-       extend: {
-         borderWidth:{
-           6: '6px'
-         },
-         colors:{
-           'cyan':colors.cyan,
-           'emerald': colors.emerald
-         }
-       },
-     },
-     variants: {},
-     plugins: [],
-}
+  purge: ["./src/**/*.js", "./dist/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      borderWidth: {
+        6: "6px",
+      },
+      colors: {
+        cyan: colors.cyan,
+        emerald: colors.emerald,
+      },
+      keyframes: {
+        wave: {
+          "0%": { transform: "rotate(0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "12%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10deg)" },
+          "60%": { transform: "rotate(0deg)" } /*Reset for last half to pause*/,
+          "100%": { transform: "rotate(0deg)" },
+        },
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        wave: "wave 2s ease-in-out infinite",
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,31 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // extract css to files
+
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-    },
-    module: {
-        rules: [
-            {
-            test: /\.css$/i,
-            use: ["style-loader","css-loader","postcss-loader"],
-            },
+  mode: isDev ? 'development' : 'production',
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader"
         ],
-    }
-}
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "assets/styles/main.css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
