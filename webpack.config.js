9e9eecbefb6343b21907f758a1e6cf436b2c0a78
Shubham Yadav <shubhamyadav.70007@outlook.com>
2023-07-23 03:49:03 +0530

    chore: webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // extract css to files
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
+const paths = {
+  src: path.resolve(__dirname, 'src'),
+  build: path.resolve(__dirname, 'dist'),
+}
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
-  entry: "./src/index.js",
+  entry: {
+    main: './src/index.js'
+  },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: isDev ? '[name].[chunkhash].js' : '[name].js',
+    path: paths.build,
+    publicPath: '/'
+  },
+  devtool: isDev ? 'inline-source-map' : false,
+  devServer: {
+    contentBase: paths.src
   },
   module: {
     rules: [
@@ -24,8 +38,39 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "assets/styles/main.css",
-      chunkFilename: "[id].css",
+      filename: 'styles/[name].[chunkhash].css',
+      chunkFilename: '[id].[chunkhash].css'
     }),
-  ],
+    !isDev ? new CleanWebpackPlugin({}) : false,
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: `${paths.src}/assets`,
+          to: 'assets',
+          noErrorOnMissing: true
+        }
+      ]
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ].filter(Boolean),
+  cache: {
+    type: "filesystem",
+  }, 
+  optimization: {
+    minimize: true,
+    minimizer: [
+      `...`, // syntax to extend existing minimizers (i.e. `terser-webpack-plugin`)
+    ],
+    // Once your build outputs multiple chunks, this option will ensure they share the webpack runtime
+    // instead of having their own. This also helps with long-term caching, since the chunks will only
+    // change when actual code changes, not the webpack runtime.
+    runtimeChunk: {
+      name: 'runtime'
+    }
+  },
+  performance: {
+    hints: false
+  }
 };
